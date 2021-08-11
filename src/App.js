bf02c52b29bf35c8f214c4ecd6ad339a6be4fe22
gopartner <smartfrenpaketansatu@gmail.com>
2021-08-11 07:43:33 +0700

    edit App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          G.O.S <br> COMMUNITY
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gopartner.github.io/app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
