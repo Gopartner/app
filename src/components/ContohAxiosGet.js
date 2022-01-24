1c1483eedf22d4aff6cc14ede7d1573bb31b41ac
gopartner <smartfrenpaketansatu@gmail.com>
2022-01-25 05:19:35 +0700

    tambah package axios dan react chat engine

---
+++ src/components/ContohAxiosGet.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import axios from 'axios';
+
+//Membuat Permintaan GET
+export default class ContohAxiosGet extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
+
