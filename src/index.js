070cb5ba23c3110e997c1b0b339343e6c42e461e
Gopartner <smartfrenpaketansatu@gmail.com>
2022-07-12 13:34:50 +0700

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './myStyle.css';
-
-stateFullCompo(10,9);
-function stateFullCompo(a,b){
-	let result = a % b;
-	return (
-		<div className="modulus">
-		 {result}
-		</div>
-	)
-}
+import App from './App.js';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <stateFullCompo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
